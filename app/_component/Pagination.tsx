2005e1695d683fb937be9f8772a9cd8e83be1fdb
tetinowa <tmrgaunt@example.com>
2025-12-02 12:58:32 +0800

    aasadasd

--- app/_component/Pagination.tsx
+++ app/_component/Pagination.tsx
@@ -6,37 +6,66 @@ import {
   PaginationLink,
   PaginationNext,
   PaginationPrevious,
-} from "@/src/components/ui/pagination"
+} from "@/src/components/ui/pagination";
+import { useParams } from "next/navigation";
+import { useState, useEffect } from "react";
 
 type Pagination1Props = {
-  className?: string
-}
+  className?: string;
+};
 
 export function Pagination1({ className }: Pagination1Props) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [toalPages, setTotalPages] = useState(1);
+  const { categoryName } = useParams<Params>();
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(
+        `${process.env.TMDB_BASE_URL}/movie/${categoryName}?language=en-US&page=1`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMTVkZDM3MjkzOTUyNTQyMTY0MjVjMDhmNDE4NWUxMSIsIm5iZiI6MTc2MzUyMzY0NC41MjMsInN1YiI6IjY5MWQzYzNjNmNjNDMzNzcxZTJkNDEwYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kkj8IUzuYcVwEahuxYgQAmfdV46-3VWJP3Rbm6rIUJ8`,
+          },
+        }
+      );
+      const data = await res.json();
+
+      setMovies(data.results);
+    };
+    fetchData();
+  },
+
   return (
     <Pagination className={className}>
       <PaginationContent>
         <PaginationItem>
           <PaginationPrevious href="#" />
         </PaginationItem>
-        <PaginationItem>
-          <PaginationLink href="#">1</PaginationLink>
-        </PaginationItem>
+
         <PaginationItem>
           <PaginationLink href="#" isActive>
-            2
+            1
           </PaginationLink>
         </PaginationItem>
+
         <PaginationItem>
-          <PaginationLink href="#">3</PaginationLink>
+          <PaginationLink href="#">2</PaginationLink>
         </PaginationItem>
+
         <PaginationItem>
           <PaginationEllipsis />
         </PaginationItem>
+
+        <PaginationItem>
+          <PaginationLink href="#">5</PaginationLink>
+        </PaginationItem>
+
         <PaginationItem>
           <PaginationNext href="#" />
         </PaginationItem>
       </PaginationContent>
     </Pagination>
-  )
+  );
 }
--- app/category/[categoryName]/page.tsx
+++ app/category/[categoryName]/page.tsx
@@ -1,9 +1,8 @@
-"use client"
+"use client";
 
 import { MovieSection } from "@/app/_component/MovieSection";
 import { use } from "react";
 import { Pagination1 } from "@/app/_component/Pagination";
-import { Pagination } from "@/src/components/ui/pagination";
 import { Header } from "@/app/_component/Header";
 import { Footer } from "@/app/_component/Footer";
 
@@ -15,26 +14,23 @@ export default function CategoryPage({ params }: { params: Promise<Params> }) {
   const { categoryName } = use(params);
   const formatTitle = (name: string) => {
     return name
-      .split('_')
-      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(' ');
+      .split("_")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
   };
 
   return (
     <div className="min-h-screen bg-white">
-    
       <Header />
-
-      <div><MovieSection
+      <MovieSection
         categoryName={categoryName}
         title={formatTitle(categoryName)}
         showButton={false}
       />
-
-      <Pagination1 className="flex justify-end"/>
-
-      <Footer /></div>
-      
+      <div className="max-w-7xl mx-auto px-4 pb-8">
+        <Pagination1 className="flex justify-end" />
+      </div>
+      <Footer />
     </div>
   );
 }
